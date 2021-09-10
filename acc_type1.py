3ab752fd79b76ac11db1da42fd8b6239ba9ac531
AlexandreSato <66435071+AlexandreSato@users.noreply.github.com>
2021-09-10 15:00:12 -0300

    Finishing put the relay on again

--- acc_type1.py
+++ acc_type1.py
@@ -1,23 +1,48 @@
 #!/usr/bin/env python3
+import sys
+import os
 from panda import Panda
 from opendbc.can.packer import CANPacker
 
-dbc_name ='toyota_nodsu_hybrid_pt_generated'
+
+max = 500 # increase this if u need more time to start car engine
+dbc_name ='toyota_nodsu_hybrid_pt_generated' # put you correct odbc here (search in values.py)
+
+p = Panda()
+p.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
+
+dumpsafety = p.health()
+if dumpsafety['safety_mode'] == 0:
+  print('Sorry, u need change for some dev branch, if u deserv this copy and past this command:\n\n\ncd ..; rm -rf openpilot; git clone -b master --recurse-submodules https://github.com/commaai/openpilot; reboot')
+  exit(1)
+
 packer = CANPacker(dbc_name)
+values = {
+  "ACC_TYPE": 1,
+}
+data = packer.make_can_msg("ACC_CONTROL", 0, values) # maybe u need change this ACC_CONTROL confirm in your odbc
 
-if __name__ == "__main__":
-    p = Panda()
-    p.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
-    dumpsafety = p.health()
-    print(f"\nsafety_mode: {dumpsafety['safety_mode']}")
-    print("If safety mode == 0? so ALLOUTPUT don't worked OR FLAG ALLOWDEBUG is missing\non Panda flash")
+def main():
 
-    values = {
-      "ACC_TYPE": 1,
-    }
+  for i in range (0, max):
+    print(f"PCM exploit :{data}")
+    print(' __      __                                  .___\n/  \    /  \ ____     ____   ____   ____   __| _/\n\   \/\/   // __ \   /    \_/ __ \_/ __ \ / __ | \n \        /\  ___/  |   |  \  ___/\  ___// /_/ | \n  \__/\  /  \___  > |___|  /\___  >\___  >____ | \n       \/       \/       \/     \/     \/     \/ \n  _________        ________ \n /   _____/ ____  /  _____/ \n \_____  \ /    \/   \  ___ \n /        \   |  \    \_\  \\\n/_______  /___|  /\______  /\n        \/     \/        \/ \n')
+    p.can_send(data[0], data[2], data[3])
+    progress(i, max, 'start your engine before reach the end')
+    if i < (max  - 1) :
+      os.system('clear')
+  
+  p.set_safety_mode(Panda.SAFETY_TOYOTA)
+  p.send_heartbeat()
+  print('relay ON again\nkthxbay\n')
 
-    data = packer.make_can_msg("ACC_CONTROL", 0, values)
-   
-    while 1:
-      p.can_send(data[0], data[2], data[3])
-      print(data)
+
+def progress(count, total, status=''):
+  bar_len = 50
+  filled_len = int(round(bar_len * count / float(total)))
+
+  bar = '#' * filled_len + '-' * (bar_len - filled_len)
+
+  sys.stdout.write('count :%s of max :%s\n [%s] \n...%s' % (count, total, bar, status))
+
+main()
